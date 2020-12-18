fc6d52a49ca95763ced074a92c70c8b7c72d3178
holamishamigos <72602335+ElizaSkrabucha@users.noreply.github.com>
2020-12-18 19:45:51 +0100

    Zajęcia 18_12_2020

---
+++ 18_12_Platform_character/platform_character/platform_character/PlatformCharacter.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+export (int) var speed = 1200
+export (int) var jump_speed = -1800
+export (int) var gravity = 4000
+export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var acceleration = 0.25
+
+var velocity = Vector2.ZERO
+
+func get_input():
+	var dir = 0
+	if Input.is_action_pressed("walk_right"):
+		dir += 1
+	if Input.is_action_pressed("walk_left"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+	
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
+		
+	
