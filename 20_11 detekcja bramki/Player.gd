60cf3cf2c294320610a14209e8d97c377d4ea1ee
holamishamigos <72602335+ElizaSkrabucha@users.noreply.github.com>
2021-01-08 19:48:55 +0100

    Porządki w zadaniach z 08_01
    Zadanie05 na zadaniuA.
    Muszę zcomittować wszytko, bo niestety github nie oddziela plików, w których wprowadzałam zmiany, od tych starszych.

---
+++ 20_11 detekcja bramki/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left")
+	if Input.is_action_pressed("ui_right")
+	if Input.is_action_pressed("ui_up")
+	if Input.is_action_pressed("ui_down")
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
